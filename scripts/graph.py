58a06f43517a00fec142ed1c17603a6fed6996b4
Jake Tronge <jtronge3@gmail.com>
2023-07-13 10:54:02 -0600

    Add options for the benchmark type in graph script

--- scripts/graph.py
+++ scripts/graph.py
@@ -2,9 +2,10 @@ import matplotlib.pyplot as plt
 import argparse
 
 graph_types = ['box-plot', 'line-graph']
+benchmarks = ['osu_mbw_mr', 'osu_latency']
 
 parser = argparse.ArgumentParser(description='generate graphs for results')
 parser.add_argument('-r', '--results-path', required=True, help='path to results dir')
-parser.add_argument('-b', '--benchmark', required=True, help='benchmark name')
+parser.add_argument('-b', '--benchmark', required=True, choices=benchmarks, help='benchmark name')
 parser.add_argument('-g', '--graph', required=True, choices=graph_types, help='graph type')
 args = parser.parse_args()
