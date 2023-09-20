b6d4cbe16a08ed00a4c753931e4b548e37c9cdd3
Jake Tronge <jtronge3@gmail.com>
2023-09-20 14:40:06 -0600

    Add initial overhead computation to graph.py

--- scripts/graph.py
+++ scripts/graph.py
@@ -8,7 +8,7 @@ graph_types = ['box-plot', 'line-graph']
 benchmarks = ['osu_mbw_mr', 'osu_latency']
 versions = ['sm', 'rsm']
 sm_fmt = 'b-'
-rsm_fmt = 'r--'
+rsm_fmt = 'r-.'
 
 parser = argparse.ArgumentParser(description='generate graphs for results')
 parser.add_argument('-r', '--results-path', required=True, help='path to results dir')
@@ -82,16 +82,58 @@ rsm_bw, rsm_bw_err = compute_error(rsm_bws)
 sm_mr, sm_mr_err = compute_error(sm_mrs)
 rsm_mr, rsm_mr_err = compute_error(rsm_mrs)
 
-# Compute overhead
-bw_overhead = 100 * ((sm_bw - rsm_bw) / sm_bw)
-print('bandwidth overhead:', np.average(bw_overhead, 0))
-mr_overhead = 100 * ((sm_mr - rsm_mr) / sm_mr)
-print('mr overhead:', np.average(mr_overhead, 0))
+
+def min_max_overhead(baseline, result):
+    """Compute min and max overhead points."""
+    overhead = 100 * ((baseline - result) / baseline)
+    # print([int(np.log2(sz)) for sz in sizes])
+    # print(overhead)
+    avg = np.average(overhead, 0)
+    print(f'-- average overhead = {avg:.2f}%')
+    mini = np.argmin(overhead)
+    power = int(np.log2(sizes[mini]))
+    print(f'-- min overhead (2^{power}) =', '{:.2f}%'.format(overhead[mini]))
+    maxi = np.argmax(overhead)
+    power = int(np.log2(sizes[maxi]))
+    print(f'-- max overhead (2^{power}) =', '{:.2f}%'.format(overhead[maxi]))
+    return (mini, overhead[mini]), (maxi, overhead[maxi]), avg
+
+# Compute overhead for bandwidth and message rate
+print('bandwidth overhead:')
+print('================================')
+print('message rate overhead:')
+min_max_overhead(sm_mr, rsm_mr)
+print('================================')
 
 # Set plot style
 plt.style.use('./paper.mplstyle')
 
+
+def compute_and_annotate_overhead(baseline, result, ax, miny, maxy, bestx=None, worstx=None, habest='center', haworst='center', flip=False):
+    """Compute and annotate the overhead."""
+    (min_overhead_idx, min_overhead), (max_overhead_idx, max_overhead), avg_overhead = min_max_overhead(baseline, result)
+
+    def annotate(text, overhead, xy, xytext, ha):
+        ax.annotate(f'{text}: {overhead:.2f}%',
+                    xy=xy, xytext=xytext,
+                    arrowprops=dict(headwidth=8.0, width=4.0, facecolor='black', shrinkB=0.7),
+                    ha=ha, fontweight='bold')
+
+    bestx = sizes[min_overhead_idx] if bestx is None else bestx
+    annotate('best', min_overhead,
+             xy=(sizes[min_overhead_idx], result[min_overhead_idx] if not flip else baseline[min_overhead_idx]),
+             xytext=(bestx, miny(min_overhead_idx)),
+             ha=habest)
+    worstx = sizes[max_overhead_idx] if worstx is None else worstx
+    annotate('worst', max_overhead,
+             xy=(sizes[max_overhead_idx], result[max_overhead_idx] if not flip else baseline[max_overhead_idx]),
+             xytext=(worstx, maxy(max_overhead_idx)),
+             ha=haworst)
+
+
+# Bandwidth
 fig, ax = plt.subplots()
+compute_and_annotate_overhead(sm_bw, rsm_bw, ax, miny=lambda i: 3 * rsm_bw[i] / 4, maxy=lambda i: 20 * rsm_bw[i])
 # print(sm_size, sm_bw)
 ax.errorbar(sizes, sm_bw, yerr=sm_bw_err, fmt=sm_fmt, label='sm')
 ax.errorbar(sizes, rsm_bw, yerr=rsm_bw_err, fmt=rsm_fmt, label='rsm')
@@ -103,7 +145,9 @@ ax.set_xscale('log', base=2)
 ax.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
 plt.show()
 
+# Message rate
 fig, ax = plt.subplots()
+compute_and_annotate_overhead(sm_mr, rsm_mr, ax, miny=lambda i: 50 * rsm_mr[i], maxy=lambda i: 5 * rsm_mr[i] / 8)
 ax.errorbar(sizes, sm_mr, yerr=sm_mr_err, fmt=sm_fmt, label='sm')
 ax.errorbar(sizes, rsm_mr, yerr=rsm_mr_err, fmt=rsm_fmt, label='rsm')
 ax.legend()
@@ -126,10 +170,14 @@ for run in range(args.count):
 sm_lat, sm_lat_err = compute_error(sm_lats)
 rsm_lat, rsm_lat_err = compute_error(rsm_lats)
 
-lat_overhead = 100 * ((rsm_lat - sm_lat) / rsm_lat)
-print('latency overhead:', np.average(lat_overhead))
+print('latency overhead:')
+min_max_overhead(rsm_lat, sm_lat)
+print('================================')
 
+# Latency
 fig, ax = plt.subplots()
+compute_and_annotate_overhead(rsm_lat, sm_lat, ax, miny=lambda i: sm_lat[i] / 8,
+                              maxy=lambda i: 4 * sm_lat[i], habest='right', flip=True)
 ax.errorbar(sizes, sm_lat, yerr=sm_lat_err, fmt=sm_fmt, label='sm')
 ax.errorbar(sizes, rsm_lat, yerr=rsm_lat_err, fmt=rsm_fmt, label='rsm')
 ax.legend()
